cb46315893cbc7ee86d4e252eebf9c3ce9ebe19b
Lucas Lu <lucas89901@gmail.com>
2023-05-25 21:18:38 +0800

    Finish v0.1.0

---
+++ fetch_daily.py
@@ -0,0 +1,100 @@
+import json
+from typing import Any
+from pathlib import Path
+import shutil
+
+import requests
+
+
+def write_to_file(path, obj) -> None:
+    with open(path, 'w', encoding='utf-8') as f:
+        f.write(obj)
+
+
+class ApiError(Exception):
+    pass
+
+
+class LeetCodeApi:
+
+    def __init__(self):
+        self.session = requests.Session()
+        self.session.get("https://leetcode.com/problemset/all/")
+        self.headers = {
+            "Referer": "https://leetcode.com/problemset/javascript/",
+            "X-csrftoken": self.session.cookies.get("csrftoken"),
+        }
+
+        with open("endpoints.json", 'r', encoding="utf-8") as f:
+            self.endpoints = json.load(f)
+
+    def send_graphql_post(
+            self,
+            endpoint: str,
+            variables: dict[str, Any] | None = None) -> dict[str, Any]:
+        payload = self.endpoints[endpoint]
+        payload["variables"] = variables
+        response = self.session.post("https://leetcode.com/graphql/",
+                                     json=payload,
+                                     headers=self.headers)
+        data = json.loads(response.text)
+
+        if "errors" in data:
+            raise ApiError(data["errors"])
+        return data["data"]
+
+    def question_title(self, title_slug: str) -> str:
+        data = self.send_graphql_post("questionTitle",
+                                      {"titleSlug": title_slug})
+        return data["question"]["title"]
+
+    def question_id(self, title_slug: str) -> str:
+        data = self.send_graphql_post("questionTitle",
+                                      {"titleSlug": title_slug})
+        return data["question"]["questionFrontendId"]
+
+    def default_code(self,
+                     title_slug: str,
+                     lang_slug: str | None = "cpp") -> str:
+        data = self.send_graphql_post("questionEditorData",
+                                      {"titleSlug": title_slug})
+        snippet = next(x for x in data["question"]["codeSnippets"]
+                       if x["langSlug"] == lang_slug)
+        return snippet["code"]
+
+
+def main():
+    api = LeetCodeApi()
+    daily_slug = api.send_graphql_post("questionOfTheDay")[
+        "activeDailyCodingChallengeQuestion"]["question"]['titleSlug']
+
+    Path("../4. Median of Two Sorted Arrays.cpp").unlink()
+    daily_slug = "median-of-two-sorted-arrays"  # For testing.
+
+    # Generate solution cpp file
+    solution_cpp = Path("../"
+                        f"{api.question_id(daily_slug)}. "
+                        f"{api.question_title(daily_slug)}.cpp")
+    if solution_cpp.exists():
+        raise FileExistsError(f"Solution file \"{solution_cpp.name}\" already "
+                              "exists.")
+    with open(solution_cpp, "w", encoding="utf-8") as f:
+        f.write(api.default_code(daily_slug))
+
+    # Generate main.cpp
+    main_cpp = Path("../main.cpp")
+    if main_cpp.exists():
+        shutil.copy2(main_cpp, "../main_old.cpp")
+    shutil.copyfile("main_template.cpp", main_cpp)
+
+    # TODO: Auto generate input variables and test cases
+    with open(main_cpp, "r+", encoding="utf-8") as f:
+        lines = f.readlines()
+        for i in range(len(lines)):
+            lines[i] = lines[i].replace("@SOLUTION_SOURCE@", solution_cpp.name)
+        f.seek(0)
+        f.writelines(lines)
+
+
+if __name__ == '__main__':
+    main()
---
+++ leetcode_util.h
@@ -0,0 +1,34 @@
+#ifndef LEETCODE_UTIL_H_
+#define LEETCODE_UTIL_H_
+
+namespace leetcode_util {
+#pragma GCC diagnostic ignored "-Wshadow"
+struct ListNode {
+  int val;
+  ListNode *next;
+  ListNode() : val(0), next(nullptr) {}
+  ListNode(int x) : val(x), next(nullptr) {}
+  ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+#pragma GCC diagnostic pop
+
+ListNode *BuildList(const size_t size) {
+  vector<ListNode *> nodes(size);
+  nodes[0] = new ListNode(1);
+  for (size_t i = 1; i < size; ++i) {
+    nodes[i] = new ListNode(i + 1);
+    nodes[i - 1]->next = nodes[i];
+  }
+  return nodes[0];
+}
+
+void PrintList(ListNode *head) {
+  cerr << '{';
+  for (ListNode *node = head; node; node = node->next) {
+    cerr << node->val << (node->next ? " -> " : "");
+  }
+  cerr << '}' << endl;
+}
+}  // namespace leetcode_util
+
+#endif  // LEETCODE_UTIL_H_
