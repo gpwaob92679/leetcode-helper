0b4f4740aead73550183abbfdb41ccee2515522a
Lucas Lu <lucas89901@gmail.com>
2023-08-06 10:53:06 +0800

    Move endpoints to class constants

--- fetch_daily.py
+++ fetch_daily.py
@@ -1,9 +1,7 @@
-import json
 from pathlib import Path
 import shutil
-from typing import Any
 
-import requests
+import leetcode_api
 
 
 def write_to_file(path, obj) -> None:
@@ -11,60 +9,8 @@ def write_to_file(path, obj) -> None:
         f.write(obj)
 
 
-class ApiError(Exception):
-    pass
-
-
-class LeetCodeApi:
-
-    def __init__(self):
-        self.session = requests.Session()
-        self.session.get('https://leetcode.com/problemset/all/')
-        self.headers = {
-            'Referer': 'https://leetcode.com/problemset/javascript/',
-            'X-csrftoken': self.session.cookies.get('csrftoken'),
-        }
-
-        with open('endpoints.json', 'r', encoding='utf-8') as f:
-            self.endpoints = json.load(f)
-
-    def send_graphql_post(
-            self,
-            endpoint: str,
-            variables: dict[str, Any] | None = None) -> dict[str, Any]:
-        payload = self.endpoints[endpoint]
-        payload['variables'] = variables
-        response = self.session.post('https://leetcode.com/graphql/',
-                                     json=payload,
-                                     headers=self.headers)
-        data = json.loads(response.text)
-
-        if 'errors' in data:
-            raise ApiError(data['errors'])
-        return data['data']
-
-    def question_title(self, title_slug: str) -> str:
-        data = self.send_graphql_post('questionTitle',
-                                      {'titleSlug': title_slug})
-        return data['question']['title']
-
-    def question_id(self, title_slug: str) -> str:
-        data = self.send_graphql_post('questionTitle',
-                                      {'titleSlug': title_slug})
-        return data['question']['questionFrontendId']
-
-    def default_code(self,
-                     title_slug: str,
-                     lang_slug: str | None = 'cpp') -> str:
-        data = self.send_graphql_post('questionEditorData',
-                                      {'titleSlug': title_slug})
-        snippet = next(x for x in data['question']['codeSnippets']
-                       if x['langSlug'] == lang_slug)
-        return snippet['code']
-
-
 def main():
-    api = LeetCodeApi()
+    api = leetcode_api.LeetCodeApi()
     daily_slug = api.send_graphql_post('questionOfTheDay')[
         'activeDailyCodingChallengeQuestion']['question']['titleSlug']
     print(f'{daily_slug = }')
---
+++ leetcode_api.py
@@ -0,0 +1,90 @@
+import json
+from typing import Any
+
+import requests
+
+
+class LeetCodeApi:
+
+    def __init__(self):
+        self.session = requests.Session()
+        self.session.get('https://leetcode.com/problemset/all/')
+        self.session.headers['Referer'] = 'https://leetcode.com/problemset/all/'
+        self.session.headers['X-csrftoken'] = self.session.cookies.get(
+            'csrftoken')
+
+    def send_graphql_post(
+            self,
+            endpoint: str,
+            variables: dict[str, Any] | None = None) -> dict[str, Any]:
+        payload = getattr(LeetCodeApiEndpoints, endpoint)
+        payload['variables'] = variables
+        response = self.session.post('https://leetcode.com/graphql/',
+                                     json=payload)
+        data = json.loads(response.text)
+
+        if 'errors' in data:
+            raise LeetCodeApiError(data['errors'])
+        return data['data']
+
+    def question_title(self, title_slug: str) -> str:
+        data = self.send_graphql_post('questionTitle',
+                                      {'titleSlug': title_slug})
+        return data['question']['title']
+
+    def question_id(self, title_slug: str) -> str:
+        data = self.send_graphql_post('questionTitle',
+                                      {'titleSlug': title_slug})
+        return data['question']['questionFrontendId']
+
+    def default_code(self,
+                     title_slug: str,
+                     lang_slug: str | None = 'cpp') -> str:
+        data = self.send_graphql_post('questionEditorData',
+                                      {'titleSlug': title_slug})
+        snippet = next(x for x in data['question']['codeSnippets']
+                       if x['langSlug'] == lang_slug)
+        return snippet['code']
+
+
+class LeetCodeApiEndpoints:
+
+    # yapf: disable
+    questionOfTheDay = {
+        'query': '\n    query questionOfToday {\n  activeDailyCodingChallengeQuestion {\n    date\n    userStatus\n    link\n    question {\n      acRate\n      difficulty\n      freqBar\n      frontendQuestionId: questionFrontendId\n      isFavor\n      paidOnly: isPaidOnly\n      status\n      title\n      titleSlug\n      hasVideoSolution\n      hasSolution\n      topicTags {\n        name\n        id\n        slug\n      }\n    }\n  }\n}\n    ',
+        'variables': {},
+        'operationName': 'questionOfToday'
+    }
+    questionTitle = {
+        'query': '\n    query questionTitle($titleSlug: String!) {\n  question(titleSlug: $titleSlug) {\n    questionId\n    questionFrontendId\n    title\n    titleSlug\n    isPaidOnly\n    difficulty\n    likes\n    dislikes\n  }\n}\n    ',
+        'variables': {
+            'titleSlug': 'TITLE_SLUG'
+        },
+        'operationName': 'questionTitle'
+    }
+    questionEditorData = {
+        'query': '\n    query questionEditorData($titleSlug: String!) {\n  question(titleSlug: $titleSlug) {\n    questionId\n    questionFrontendId\n    codeSnippets {\n      lang\n      langSlug\n      code\n    }\n    envInfo\n    enableRunCode\n  }\n}\n    ',
+        'variables': {
+            'titleSlug': 'TITLE_SLUG'
+        },
+        'operationName': 'questionEditorData'
+    }
+    questionContent = {
+        'query': '\n    query questionContent($titleSlug: String!) {\n  question(titleSlug: $titleSlug) {\n    content\n    mysqlSchemas\n  }\n}\n    ',
+        'variables': {
+            'titleSlug': 'TITLE_SLUG'
+        },
+        'operationName': 'questionContent'
+    }
+    consolePanelConfig = {
+        'query': '\n    query consolePanelConfig($titleSlug: String!) {\n  question(titleSlug: $titleSlug) {\n    questionId\n    questionFrontendId\n    questionTitle\n    enableDebugger\n    enableRunCode\n    enableSubmit\n    enableTestMode\n    exampleTestcaseList\n    metaData\n  }\n}\n    ',
+        'variables': {
+            'titleSlug': 'TITLE_SLUG'
+        },
+        'operationName': 'consolePanelConfig'
+    }
+    # yapf: enable
+
+
+class LeetCodeApiError(Exception):
+    pass
